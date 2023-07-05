6833bee9350426ff991a3774a5594e9e9767b0da
Loreanna <s.s.khovanskaya@gmail.com>
2023-07-06 01:19:55 +0300

    requirements changed

--- client.py
+++ client.py
@@ -1,21 +1,21 @@
 import requests
 
 # POST
-# data = requests.post('http://127.0.0.1:5000/advert/', json={'title': '22', 'username': '22', 'content': '22'})
-# print(data.status_code)
-# print(data.text)
+data = requests.post('http://127.0.0.1:5000/advert/', json={'title': '23', 'username': '232', 'content': '232'})
+print(data.status_code)
+print(data.text)
 
 # GET
 
-# data = requests.get('http://127.0.0.1:5000/advert/4/')
+# data = requests.get('http://127.0.0.1:5000/advert/5/')
 # print(data.status_code)
 # print(data.text)
 
 # PATCH
 
-data = requests.patch('http://127.0.0.1:5000/advert/4/', json={'title': 'patched1'})
-print(data.status_code)
-print(data.text)
+# data = requests.patch('http://127.0.0.1:5000/advert/1/', json={'title': 'patched1'})
+# print(data.status_code)
+# print(data.text)
 
 # DELETE
 
